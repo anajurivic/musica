4f3902d0ad2ff1b6eab02d2ab4726d9ba4ea413a
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-12 22:26:05 -0300

    exercicio concluido

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Como É Grande O Meu Amor Por Você\n");
+Console.ResetColor();
+
+Console.WriteLine("\nEu tenho tanto pra lhe falar");
+Console.WriteLine("Mas com palavras não sei dizer");
+Console.WriteLine("Como é grande o meu amor por você\n");
+
+Console.WriteLine("\nE não há nada pra comparar");
+Console.WriteLine("Para poder lhe explicar");
+Console.WriteLine("Como é grande o meu amor por você\n");
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("\nNem mesmo o céu nem as estrelas");
+Console.WriteLine("Nem mesmo o mar e o infinito");
+Console.WriteLine("Nada é maior que o meu amor");
+Console.WriteLine("Nem mais bonito\n");
+Console.ResetColor();
+
+Console.WriteLine("\nMe desespero a procurar");
+Console.WriteLine("Alguma forma de lhe falar");
+Console.WriteLine("Como é grande o meu amor por você\n");
+
+Console.WriteLine("\nNunca se esqueça nem um segundo");
+Console.WriteLine("Que eu tenho o amor maior do mundo");
+Console.WriteLine("Como é grande o meu amor por você\n");
+
+Console.WriteLine("\nNunca se esqueça, nem um segundo");
+Console.WriteLine("Que eu tenho o amor maior do mundo");
+Console.WriteLine("Que eu tenho o amor maior do mundo\n");
+
+Console.WriteLine("\nMas como é grande o meu amor por você\n");
